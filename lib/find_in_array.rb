906fcffb88471839d7572f5c8c5107cb04e27c72
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-12 10:15:34 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  counter == 0 
-  while array[counter] do 
+  counter == 0
+  while array[counter] do
     array[counter] == value_to_find
-    if (array[counter] == value_to_find) == true 
-      break 
-    else 
+    if (array[counter] == value_to_find) == true
+      break
+    else
     end
 end
